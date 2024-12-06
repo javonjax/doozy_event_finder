15e01739fad507f399becf5cc6fa14e6f9368df9
javonjax <javonj461@hotmail.com>
2024-12-06 15:25:01 -0500

    Removed duplicate types and interfaces that were remade with Zod.

--- backend/src/schemas/schemas.ts
+++ backend/src/schemas/schemas.ts
@@ -1,47 +1,12 @@
 import { z } from 'zod';
 
-export interface ApiData {
-    _embedded: {
-        events: Event[]
-    }
-}
-
-export interface Event {
-    name: string,
-    id: string,
-    dates: {
-        start: {
-            localDate?: string,
-            localTime?: string,
-            dateTime?: string
-        }
-    },
-    priceRanges: {
-        currency?: string,
-        min?: number,
-        max?: number
-    },
-    _embedded:{
-        venues?: {
-            name?: string,
-            id?: string,
-            images?: any,
-            city: {
-                name?: string
-            },
-            state: {
-                name?: string
-            }
-        }[]
-    },
-    [index: string]: unknown
-}
 export const Venue = z.object({
     name: z.string(),
     address: z.record(z.string(), z.string()),
     city: z.record(z.string(), z.string()),
     state: z.record(z.string(), z.string())
 });
+
 /*
     /events route
     Contains general information about events to be displayed on cards
