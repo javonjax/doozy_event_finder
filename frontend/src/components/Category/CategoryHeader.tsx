7a302f2073147f346af5ffd39708f6dde5c8d371
javonjax <javonj461@hotmail.com>
2024-12-30 20:11:40 -0500

    Updated how category header images are loaded. Added low quality versions of images to be displayed while loading the higher quality version.

--- frontend/src/components/Category/CategoryHeader.tsx
+++ frontend/src/components/Category/CategoryHeader.tsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
+
 const CategoryHeader = (): React.JSX.Element => {
   const path: string = useLocation().pathname.slice(1).toLowerCase();
+  const lowQualityImg: string = `${path}_header_lq.webp`;
+  const highQualityImg: string = `${path}_header.webp`;
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  
+  useEffect(() => {
+    setIsLoaded(false);
+    const img = new Image();
+    img.src = highQualityImg;
+    img.onload = () => {
+      setIsLoaded(true);
+    };
+  }, [highQualityImg]);
+
   return (
-    <div
-      className='w-full h-[250px] relative overflow-hidden rounded-2xl my-4 bg-cover bg-center'
-      style={{backgroundImage: `url('${path}_header.webp')`,}}
-    >
+    <div className='w-full h-[250px] relative overflow-hidden rounded-2xl my-4 bg-cover bg-center'>
+      <img
+        src={isLoaded ? highQualityImg : lowQualityImg}
+        alt={`${path} category header image.`}
+        className={`w-full h-full object-cover transition-all duration-400 ${isLoaded ? 'blur-0' : 'blur-lg'}`}
+      />
       <div className='absolute top-4 left-4 bg-[hsl(var(--background))] p-4 rounded-2xl text-[hsl(var(--text-color))]'>
         <NavLink to={'/'}>Home</NavLink>
         <span> / </span>
